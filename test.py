b417487839b47812b39d961cc3366eed5a858d86
정명찬/Blake Jeong <104886103+blakej2432@users.noreply.github.com>
2023-01-25 18:21:33 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "테스트 중 입니다."
+
+@app.route("/test", methods=['POST'])
+def translate():
+    if request.method == 'POST':
+        input_json = request.get_json()
+        print(input_json['text'])
+        if input_json['text'] == "받았니?":
+            return "받았어"
+
+if __name__ == "__main__":
+    app.run(host='3.38.250.25',port='5000', debug=True)
